68b82ec16679a707a0698db77940fc1d7c11ec20
phariha <phariha@uw.edu>
2024-01-25 16:43:01 -0800

    changed code to add both meta tag decription as well as content description

--- routes/api/v1/api_v1.js
+++ routes/api/v1/api_v1.js
@@ -29,6 +29,7 @@ router.get('/urls/preview', async (req, res) => {
     let ogImage = ''
     let ogDescription = ''
     let ogAlt = ''
+    let ogDetailedDescription = ''
 
 
     // to find this information, we want to extract the values and put 
@@ -36,17 +37,20 @@ router.get('/urls/preview', async (req, res) => {
     metaTags.forEach(tag => {
       const property = tag.getAttribute('property')
       const content = tag.getAttribute('content')
+      const name = tag.getAttribute('name')
     
       if (property && property.includes('og:url')) {
         ogUrl = content || url
-      } else if (property === 'og:title') {
+      } else if (property == 'og:title') {
         ogTitle = content || ''
       } else if (property === 'og:image') {
         ogImage = content || ''
-      } else if (property === 'og:description') {
+      } else if (property == 'og:description') {
         ogDescription = content || ''
-      } else if (property === 'og:image:alt') {
+      } else if (property == 'og:image:alt') {
         ogAlt = content || ''
+      } else if (name == 'description') {
+        ogDetailedDescription = content
       }
     })
 
@@ -64,8 +68,20 @@ router.get('/urls/preview', async (req, res) => {
       }
     }
 
+    if (!ogImage) {
+      ogImage = 'No image found'
+    }
+
     if (!ogAlt) {
-      ogAlt = 'No image Description Found'
+      ogAlt = 'No image description found'
+    }
+
+    if (ogDescription === ogDetailedDescription) {
+      ogDetailedDescription = ''
+    }    
+
+    if (!ogDetailedDescription) {
+      ogDetailedDescription = ''
     }
   
 
@@ -73,6 +89,7 @@ router.get('/urls/preview', async (req, res) => {
     // console.log("Here is a title tag " + ogTitle)
     // console.log("Here is an image tag " + ogImage)
     // console.log("Here is a description tag " + ogDescription)
+    // console.log("Here is a detailed description " + ogDetailedDescription)
 
     const htmlResponse = `
       <div style="max-width: 800px; border: solid 2px #333; text-align: center; background-color: #f8f8f8; border-radius: 10px;">
@@ -82,6 +99,7 @@ router.get('/urls/preview', async (req, res) => {
         </a>
         <p style="font-size: 24px; margin-bottom: 18px; margin-top: 15px;"><strong>Image Alt Text:</strong> ${ogAlt}</p>
         <p style="font-size: 18px; margin-top: 20px;">${ogDescription}</p>
+        <p style="font-size: 18px; margin-top: 20px;">${ogDetailedDescription}</p>
       </div>
     `
 
